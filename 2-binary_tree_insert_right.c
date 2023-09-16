b17ac6dde057f871afabb1f1ceb1bd47c83a9272
maouia <maouiabend@gmail.com>
2023-09-16 14:52:48 +0100

    fix code

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,19 +20,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new_node->parent = parent;
 	new_node->n = value;
-	new_node->right = NULL;
-	new_node->right = NULL;
-
-	if (parent->right == NULL)
+	new_node->right = parent->right;
+	parent->right = new_node;
+	new_node->left = NULL;
+	if (new_node->right != NULL)
 	{
-		parent->right = new_node;
-	}
-	else
-	{
-		parent->right->parent = new_node;
-		new_node->parent = parent;
-		new_node->right = parent->right;
-		parent->right = new_node;
+		new_node->right->parent = new_node;
 	}
 	return (new_node);
 }
